010efb3007d93fa8bb92053a84f967f8db76fc7a
Elif Kaya <84388617+elifkaya3@users.noreply.github.com>
2024-03-19 15:22:10 +0300

    logo

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -7,14 +7,20 @@ const CountryCard = ({ title, capital, imageUrl }) => {
   return (
     <>
       <div className="card">
-        <img src={imageUrl} alt={title} className="country-image" />
-
+        <Link
+              to={{
+                pathname: "./country-detail",
+                search: `?title=${title}`,
+              }}
+            >
+             <img src={imageUrl} alt={title} className="country-image" />
+          </Link>
         <div className="card-alt">
           <div className="card-content">
             <span> <h3>{title}</h3> </span>
             <span>  <b>Capital: </b> {capital} </span>
           </div>
-
+         
           <div>
             <Link
               to={{
